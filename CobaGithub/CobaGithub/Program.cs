7226f1fea79fb31ac587bdddb528489967b1cf25
Lynanisa <106790481+Lynanisa@users.noreply.github.com>
2022-06-03 10:37:10 +0700

    Add files via upload

---
+++ CobaGithub/CobaGithub/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CobaGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lynatu");
+        }
+    }
+}
